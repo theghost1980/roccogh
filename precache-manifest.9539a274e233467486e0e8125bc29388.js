e459bc8a2a4b801c5b707f9561dfb73b62c82ea2
theghost1980 <saturnob612@gmail.com>
2020-10-20 11:22:19 -0400

    Updates

---
+++ precache-manifest.9539a274e233467486e0e8125bc29388.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a650d745c945fb54add65c1e43714f",
+    "url": "/theghost1980/rocco-gh/index.html"
+  },
+  {
+    "revision": "5c45a91c67f09d9d3fdb",
+    "url": "/theghost1980/rocco-gh/static/css/2.31039caa.chunk.css"
+  },
+  {
+    "revision": "fdd038d36b4c142f3e99",
+    "url": "/theghost1980/rocco-gh/static/css/main.bd21205f.chunk.css"
+  },
+  {
+    "revision": "5c45a91c67f09d9d3fdb",
+    "url": "/theghost1980/rocco-gh/static/js/2.79264902.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/theghost1980/rocco-gh/static/js/2.79264902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd038d36b4c142f3e99",
+    "url": "/theghost1980/rocco-gh/static/js/main.0f56175f.chunk.js"
+  },
+  {
+    "revision": "384af1ee0af7d57c75e4",
+    "url": "/theghost1980/rocco-gh/static/js/runtime-main.e667d145.js"
+  },
+  {
+    "revision": "02fc7434f13ee85d00e4379c8a138cce",
+    "url": "/theghost1980/rocco-gh/static/media/100px-React.svg.02fc7434.png"
+  },
+  {
+    "revision": "d34fe6a09c32ff3db1989e1ed05c3569",
+    "url": "/theghost1980/rocco-gh/static/media/AFS-logo.d34fe6a0.png"
+  },
+  {
+    "revision": "f3b62ec3e0543c347b6a176ee853c703",
+    "url": "/theghost1980/rocco-gh/static/media/AISS-logo.f3b62ec3.jpg"
+  },
+  {
+    "revision": "31e9677ba177e3db35185a52b37f45aa",
+    "url": "/theghost1980/rocco-gh/static/media/CV-English-Rocco-Mangieri.31e9677b.pdf"
+  },
+  {
+    "revision": "3df3422872fff3691d7e3eddc7e4fde0",
+    "url": "/theghost1980/rocco-gh/static/media/DOSSIER-Rocco-Mangieri.3df34228.pdf"
+  },
+  {
+    "revision": "bb32bb85326f7974e0a73b8d1c110922",
+    "url": "/theghost1980/rocco-gh/static/media/FELS-logo.bb32bb85.png"
+  },
+  {
+    "revision": "c5c1f52f9f9a0589e530266ef66dd5d7",
+    "url": "/theghost1980/rocco-gh/static/media/IASS-AIS-logo.c5c1f52f.jpg"
+  },
+  {
+    "revision": "fba3dd1e1779b81102523044c60a0319",
+    "url": "/theghost1980/rocco-gh/static/media/NotoSerifKR-Black.fba3dd1e.otf"
+  },
+  {
+    "revision": "55d8ef7a7ce510531b65239ee7ecc6d3",
+    "url": "/theghost1980/rocco-gh/static/media/NotoSerifKR-Medium.55d8ef7a.otf"
+  },
+  {
+    "revision": "cead1c02879151a3ecbda42eac6e7d38",
+    "url": "/theghost1980/rocco-gh/static/media/img_not_available.cead1c02.png"
+  },
+  {
+    "revision": "4789317c9def6ece3c501bf3e4187280",
+    "url": "/theghost1980/rocco-gh/static/media/main-3.4789317c.jpg"
+  },
+  {
+    "revision": "8ea85770542722b6c672ea4f57cd845a",
+    "url": "/theghost1980/rocco-gh/static/media/main-cover.8ea85770.png"
+  },
+  {
+    "revision": "e5703caab3bd486399ee4b3ca5c8d45a",
+    "url": "/theghost1980/rocco-gh/static/media/main-cover2.e5703caa.jpg"
+  },
+  {
+    "revision": "9da269d9b161698bc5ab4bd6bb692ad8",
+    "url": "/theghost1980/rocco-gh/static/media/main-cover3.9da269d9.jpg"
+  },
+  {
+    "revision": "6f0ab60d980311e76a0c603164cf08d5",
+    "url": "/theghost1980/rocco-gh/static/media/susan-yin-2JIvboGLeho-unsplash.6f0ab60d.jpg"
+  }
+]);
